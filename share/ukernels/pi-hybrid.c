ea3b732b1ef2f320af7ac6ea057d0d1f0db363af
Vivek Kale <vivek.lkale@gmail.com>
2020-04-23 10:04:24 -0700

    Updating with optional MPI

--- share/ukernels/pi-hybrid.c
+++ share/ukernels/pi-hybrid.c
@@ -6,21 +6,22 @@
 #ifdef _OPENMP
 #include "omp.h"            /* OpenMP header file */
 #endif
-#define NUM_STEPS 100000000
-#define MAX_THREADS 4
+#define NUM_STEPS 10000
+#define MAX_THREADS 64
 
 int main(int argc, char *argv[])
 {
   int nprocs, myid;
   int tid, nthreads, nbin;
   double start_time, end_time;
-  double pi, Psum=0.0, sum[MAX_THREADS]={0.0};
+  double pi = 0.0, Psum=0.0, sum[MAX_THREADS]={0.0};
   double step = 1.0/(double) NUM_STEPS;/* initialize for MPI */
   #ifdef USE_MPI
   MPI_Init(&argc, &argv);/* starts MPI *//* get number of processes */
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);/* get this process's number (ranges from 0 to nprocs - 1) */
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
   #else
+  myid = 0;
   nprocs = 1;
   #endif
 
@@ -39,19 +40,23 @@ int main(int argc, char *argv[])
 #endif
     for (i=nbin*myid+tid; i < nbin*(myid+1); i+= nthreads)
       {
-	/* changed*/x = (i+0.5)*step;
+	/* changed*/ x = (i+0.5)*step;
 	sum[tid] += 4.0/(1.0+x*x);
       }
    }
   for(tid=0; tid<nthreads; tid++)   /*sum by each mpi process*/
     Psum += sum[tid]*step;
-  #ifdef USE_MPI
+#ifdef USE_MPI
   MPI_Reduce(&Psum, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);/* added */
-  #endif
+#endif
   if (myid == 0)
     {
-      printf("parallel program results with %d processes:\n", nprocs);
+      printf("parallel program results with %d processes and %d threads:\n", nprocs, nthreads);
+      #ifdef USE_MPI
       printf("pi = %g  (%17.15f)\n",pi, pi);
+      #else
+      printf("pi = %g  (%17.15f)\n",Psum, Psum);
+      #endif
     }
   #ifdef USE_MPI
   MPI_Finalize();

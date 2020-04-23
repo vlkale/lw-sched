433cd5b603826ff218f77089836a31d3f175ee4f
Vivek Kale <vivek.lkale@gmail.com>
2020-04-23 09:50:47 -0700

    Adding pi hybrid

--- share/ukernels/pi-hybrid.c
+++ share/ukernels/pi-hybrid.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef USE_MPI
 #include <mpi.h>                /* MPI header file */
+#endif
 #ifdef _OPENMP
 #include "omp.h"            /* OpenMP header file */
 #endif
@@ -14,9 +16,14 @@ int main(int argc, char *argv[])
   double start_time, end_time;
   double pi, Psum=0.0, sum[MAX_THREADS]={0.0};
   double step = 1.0/(double) NUM_STEPS;/* initialize for MPI */
+  #ifdef USE_MPI
   MPI_Init(&argc, &argv);/* starts MPI *//* get number of processes */
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);/* get this process's number (ranges from 0 to nprocs - 1) */
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  #else
+  nprocs = 1;
+  #endif
+
   nbin= NUM_STEPS/nprocs;
   // MPI_OpenMP version
 #pragma omp parallel private(tid)
@@ -38,11 +45,16 @@ int main(int argc, char *argv[])
    }
   for(tid=0; tid<nthreads; tid++)   /*sum by each mpi process*/
     Psum += sum[tid]*step;
+  #ifdef USE_MPI
   MPI_Reduce(&Psum, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);/* added */
+  #endif
   if (myid == 0)
     {
       printf("parallel program results with %d processes:\n", nprocs);
       printf("pi = %g  (%17.15f)\n",pi, pi);
     }
-  MPI_Finalize();return 0;
+  #ifdef USE_MPI
+  MPI_Finalize();
+  #endif
+  return 0;
 }
